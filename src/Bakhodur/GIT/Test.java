c2d0718e88f4980eac0b1c9b9b370c16e4682d45
bakhodurtakhirov <bakhodur.takhirov@gmail.com>
2023-12-13 15:51:34 -0500

    8

--- src/Bakhodur/GIT/Test.java
+++ src/Bakhodur/GIT/Test.java
@@ -3,8 +3,10 @@ package Bakhodur.GIT;
 public class Test {
     public static void main(String[] args) {
 
+        
                 System.out.println(1);
         
         System.out.println(3);
+        System.out.println(4);
     }
 }
