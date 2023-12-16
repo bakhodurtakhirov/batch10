9e131c3fb0b93f39d0223a81c87a985923a4987b
bakhodurtakhirov <bakhodur.takhirov@gmail.com>
2023-12-15 23:17:52 -0500

    Revert "gitignore"
    This reverts commit 0dce6525

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+package Bakhodur.GIT;
+
+public class Test {
+    public static void main(String[] args) {
+                System.out.println(1);
+
+        
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);//Com
+    }
+}
