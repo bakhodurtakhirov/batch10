f0671ac837f3fb746950351fe349a7f4272ba247
bakhodurtakhirov <bakhodur.takhirov@gmail.com>
2023-12-15 23:23:56 -0500

    1

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
   
         System.out.println("Test");
         System.out.println("Test2");
-        System.out.println("Test3");
+        System.out.println("Test31");
     }
 
 }
